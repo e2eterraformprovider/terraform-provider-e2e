5acf3b888897ad04cf922a7b721b88840acd90db
sumantkumar12345 <sumant.kumar@e2enetworks.com>
2024-02-26 15:54:59 +0530

    3rd commit

--- e2e/provider.go
+++ e2e/provider.go
@@ -2,15 +2,13 @@ package e2e
 
 import (
 	"github.com/e2eterraformprovider/terraform-provider-e2e/client"
-	// "github.com/e2eterraformprovider/terraform-provider-e2e/e2e/image"
-	// "github.com/e2eterraformprovider/terraform-provider-e2e/e2e/loadbalancer"
+	"github.com/e2eterraformprovider/terraform-provider-e2e/e2e/image"
 	"github.com/e2eterraformprovider/terraform-provider-e2e/e2e/node"
 	"github.com/e2eterraformprovider/terraform-provider-e2e/e2e/sfs"
 	"github.com/e2eterraformprovider/terraform-provider-e2e/e2e/reserve_ip"
 	"github.com/e2eterraformprovider/terraform-provider-e2e/e2e/ssh_key"
-	// "github.com/e2eterraformprovider/terraform-provider-e2e/e2e/vpc"
+	"github.com/e2eterraformprovider/terraform-provider-e2e/e2e/vpc"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	
 )
 
 func Provider() *schema.Provider {
@@ -38,20 +36,18 @@ func Provider() *schema.Provider {
 			},
 		},
 		ResourcesMap: map[string]*schema.Resource{
-			"example_node":         node.ResourceNode(),
-			"example_sfs":          sfs.ResourceSfs(),
-			// "example_image":        image.ResourceImage(),
-			// "example_loadbalancer": loadbalancer.ResourceLoadBalancer(),
-			// "example_vpc":          vpc.ResouceVpc(),
+			"e2e_node":  node.ResourceNode(),
+			"e2e_image": image.ResourceImage(),
+			"e2e_sfs": sfs.ResourceSfs(),
 		},
 		DataSourcesMap: map[string]*schema.Resource{
-			"example_node":   node.DataSourceNode(),   
-			// "example_images": image.DataSourceImages(),
+			"e2e_node":   node.DataSourceNode(),
+			"e2e_images": image.DataSourceImages(),
 			//"example_security_groups": security_group.DataSourceSecurityGroups(),
-			"example_ssh_keys":    ssh_key.DataSourceSshKeys(),
-			// "example_vpcs":        vpc.DataSourceVpcs(),
-			"example_reserve_ips": reserve_ip.DataSourceReserveIps(),
-			"example_nodes":       node.DataSourceNodes(),
+			"e2e_ssh_keys":    ssh_key.DataSourceSshKeys(),
+			"e2e_vpcs":        vpc.DataSourceVpcs(),
+			"e2e_reserve_ips": reserve_ip.DataSourceReserveIps(),
+			"e2e_nodes":       node.DataSourceNodes(),
 		},
 		ConfigureFunc: providerConfigure,
 	}
@@ -62,5 +58,5 @@ func providerConfigure(d *schema.ResourceData) (interface{}, error) {
 	api_key := d.Get("api_key").(string)
 	auth_token := d.Get("auth_token").(string)
 	api_endpoint := d.Get("api_endpoint").(string)
-	return client.NewClient(api_key, auth_token, api_endpoint),nil
+	return client.NewClient(api_key, auth_token, api_endpoint), nil
 }
