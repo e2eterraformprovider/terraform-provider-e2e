0f4bd12a7c2c3a6bc6a436286430f529d77aac7f
Bharat Bardiya <bharat.bardiya@e2enetworks.com>
2024-03-13 15:33:38 +0530

    Task - DIS-I6532 ssh keys management issue

--- e2e/node/resource_node.go
+++ e2e/node/resource_node.go
@@ -440,14 +440,6 @@ func resourceUpdateNode(ctx context.Context, d *schema.ResourceData, m interface
 		}
 	}
 
-	if d.HasChange("label") {
-		log.Printf("[INFO] changed label = %s ", d.Get("label").(string))
-		_, err = apiClient.UpdateNode(nodeId, "label_rename", d.Get("label").(string), project_id)
-		if err != nil {
-			return diag.FromErr(err)
-		}
-	}
-
 	if d.HasChange("ssh_keys") {
 		prevSshKeys, _ := d.GetChange("ssh_keys")
 
